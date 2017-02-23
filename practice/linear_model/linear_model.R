b93860aa60bccefec3761a48240e22bc67ef5063
Roy Burstein <royburst@uw.edu>
2017-02-22 17:50:57 -0800

    updates

--- practice/linear_model/linear_model.R
+++ practice/linear_model/linear_model.R
@@ -1,5 +1,7 @@
-
+# Roy Burstein
 # Code modified from James Thorson
+# Also of help: https://github.com/kaskr/adcomp/wiki/Tutorial
+# Running through some intro model fitting using TMB
 rm(list=ls())
 
 library(devtools)
@@ -41,7 +43,7 @@ nll_norm <- function(par, data){
 }
 
 # example of what this returns
-nll_norm(par=c(13,2), data=list('y' = x))
+nll_norm(par=c(12,2), data=list('y' = x))
 nll_norm(par=c(10,2), data=list('y' = x))
 nll_norm(par=c(8,2),  data=list('y' = x))
 
@@ -56,59 +58,61 @@ print(sqrt(diag(solve(opt$hessian)))) # standard errors
 
 ##################################################
 ###### Method 3 -- Optimize using TMB
-# Step 1 -- make and compile template file
+# compile template file
+# file must be in the same directory as this script
 compile("linear_model_v1.cpp")
 
-# Step 2 -- build inputs and object
+# build inputs and object
 dyn.load( dynlib("linear_model_v1") )
-pars = list("mean"=0, "log_sd"=0)
-data = list("y_i"=x)
-obj = MakeADFun( data=data, parameters=pars, DLL="linear_model_v1")
 
-# Step 3 -- test and optimize
-obj$fn(obj$par)
-obj$gr(obj$par)
-opt = nlminb(start=obj$par, objective=obj$fn, gradient=obj$gr )
-opt$diagnostics = data.frame( "name"=names(obj$par), "Est"=obj$par, "final_gradient"=as.vector(obj$gr(opt$par)))
+# make an R object representing the cpp function
+# data and parameter names must correspond to those in the cpp script
+obj  <- MakeADFun(data       = list("y_i"=x),
+                  parameters = list("mean"=0, "log_sd"=0),
+                  DLL        = "linear_model_v1")
+
+# test, should correspond with the nll_norm() calls above ^^
+obj$fn(list("mean"=12, "log_sd"=log(2)))  # Call TMB function for one value
+obj$fn(list("mean"=10, "log_sd"=log(2)))  # Call TMB function for one value
+obj$fn(list("mean"=8,  "log_sd"=log(2)))  # Call TMB function for one value
+# test gradient
+obj$gr(list("mean"=12, "log_sd"=log(2))) # First order derivatives (w.r.t mu and sigma)
+obj$gr(list("mean"=10, "log_sd"=log(2))) # First order derivatives (w.r.t mu and sigma) # SHOULD BE CLOSEST TO ZERO
+obj$gr(list("mean"=8, "log_sd"=log(2))) # First order derivatives (w.r.t mu and sigma)
+# test hessian
+obj$he(list("mean"=10, "log_sd"=log(2))) # Second order derivatives, i.e. the Hessian matrix
+
+# optimize
+opt <- nlminb(start=obj$par, objective=obj$fn, gradient=obj$gr) # will marginal gradient close to 0
+opt$diagnostics <- data.frame( "name"=names(obj$par), "Est"=c, "final_gradient"=as.vector(obj$gr(opt$par)))
 opt$par # estimated parameters
-SD = sdreport(obj) # standard errors
+print(sqrt(diag(solve(obj$he(opt$par))))) # standard errors using the hessian
+sdreport(obj) # standard errors the easier way
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-############
-# Example 1 -- average CPUE for canary rockfish
+##################################################
+##################################################
+# Example 2 -- normal model with fixed effects, using TMB of course
 ############
 
-# Define a new design matrix
-X = cbind( "CA"=ifelse(WCGBTS_Canary_example$BEST_LAT_DD<42,1,0), "OR"=ifelse(WCGBTS_Canary_example$BEST_LAT_DD>=42&WCGBTS_Canary_example$BEST_LAT_DD<46,1,0), "WA"=ifelse(WCGBTS_Canary_example$BEST_LAT_DD>=46,1,0), "Pass"=WCGBTS_Canary_example$PASS-1.5 )
+# Simulate some basic data
+X <- cbind(rep(1,100),runif(n=100))
+y <- 2*X[,1] + 10*X[,2] + rnorm(n=100,mean=0,sd=2)
+summary(lm(y~X[,2]))# test
 
-# Step 1 -- make and compile template file
-compile( "linear_model_v2.cpp" )
+# compile template file
+compile("linear_model_v2.cpp")
 
-# Step 2 -- build inputs and object
-dyn.load( dynlib("linear_model_v2") )
-Params = list("b_j"=rep(0,ncol(X)), "log_sd"=0)
-Data = list( "y_i"=CPUE, "X_ij"=X )
-Obj = MakeADFun( data=Data, parameters=Params, DLL="linear_model_v2")
+# build inputs and object
+dyn.load(dynlib("linear_model_v2"))  # dynamically link the C++ code
+Params <- list("b_j"=rep(0,ncol(X)), "log_sd"=0)
+Data   <- list("y_i"=y, "X_ij"=X)
+Obj    <- MakeADFun(data=Data, parameters=Params, DLL="linear_model_v2")
 
-# Step 3 -- test and optimize
-Obj$fn( Obj$par )
-Obj$gr( Obj$par )
+# test and optimize
 Opt = nlminb( start=Obj$par, objective=Obj$fn, gradient=Obj$gr )
 Opt$diagnostics = data.frame( "name"=names(Obj$par), "Est"=Opt$par, "final_gradient"=as.vector(Obj$gr(Opt$par)))
 Opt$par # estimated parameters
-SD = sdreport( Obj ) # standard errors
+SD = sdreport(Obj) # standard errors
