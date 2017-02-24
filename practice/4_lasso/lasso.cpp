88d82c0c0df7df34e495c492a52216633eabfd71
Roy Burstein <royburst@uw.edu>
2017-02-24 14:44:50 -0800

    lasso template

--- practice/4_lasso/lasso.cpp
+++ practice/4_lasso/lasso.cpp
@@ -3,34 +3,38 @@ template<class Type>
 Type objective_function<Type>::operator() ()
 {
   // Data
-  DATA_VECTOR(y_i);
-  DATA_MATRIX(X_ij)
+  DATA_VECTOR(y);
+  DATA_MATRIX(X)
 
   // Parameters
-  PARAMETER(lambda);
+ //   PARAMETER(lambda);
   PARAMETER(log_sigma);
   PARAMETER_VECTOR(betas);
 
   // Objective funcction
   Type sd = exp(log_sd);
   Type nll = 0;
-  int n_data = y_i.size();
+  int n = y_i.size();
 
+
+  float absbeta = 0.0;
+  for(int i = 0; i < betas.size(); i++) absbeta += betas[i] * betas[i];
+  absbeta = sqrt(absbeta);
+
+  // priors
+  nll = nll - 0.2 * v.sum(absbeta)        // 0.2 is lambda. priors for betas
+  nll = nll - log(sd^2)                // prior for sigma^2
+
+  // Linear predictor
+  vector<Type> linpred_i(n);
+  linpred = X*betas;
 
   // Probability of data conditional on fixed and random effect values
-  for(int i=0; i<n_data; i++){
-    jnll -= dnorm(y(i), X0 + J(j(i)), exp(log_SD0), true);
+  for(int i=0; i<n; i++){
+    nll -= dnorm(y(i), linpred, sd, true);
   }
 
-  // Probability of random coefficients
-  for( int i=0; i<n_factors; i++){
-    jnll -= dnorm(J(i), Type(0.0), exp(log_SDJ), true);
-  }
-
-  // Reporting
-  Type SDJ = exp(log_SDJ);
-  Type SD0 = exp(log_SD0);
 
 
-  return jnll;
+  return nll;
 }
