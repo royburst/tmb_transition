db015836d48c8dbd85469215c80d0794acc91748
Roy Burstein <royburst@ihme.washington.edu>
2017-02-28 13:30:50 -0800

    gamma

--- practice/4_lasso/lasso.cpp
+++ practice/4_lasso/lasso.cpp
@@ -23,7 +23,7 @@ Type objective_function<Type>::operator() ()
 
   // priors
   using namespace density;
-  nll = nll - dlnorm(L,Type(0.0),Type(1.0), true);          // lambda prior
+  nll = nll - dgamma(L,Type(2.0),Type(2.0), true);          // lambda prior
   nll = nll + L * absbeta;             // 0.2 is lambda. priors for betas
   nll = nll + log(pow(sd,2));             // prior for sigma^2
 
