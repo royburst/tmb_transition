ccbe978dc679563c0d5604b87f1fd8911376a998
zimaaron <azimmer@uw.edu>
2017-03-13 14:51:27 -0700

    adding Type()

--- practice/5_spde/basic_spde.cpp
+++ practice/5_spde/basic_spde.cpp
@@ -59,7 +59,7 @@ Type objective_function<Type>::operator() ()
   matrix<Type> Epsilon_xt(n_x, n_t);
 
   // Priors
-  jnll_comp(3) -= dnorm(log_tau_E, 0.0, 1.0, true); // N(0,1) prior for log_tau
+  jnll_comp(3) -= dnorm(log_tau_E, Type(0.0), Type(1.0), true); // N(0,1) prior for log_tau
 
   // Probability of Gaussian-Markov random fields (GMRFs)
   jnll_comp(0) += GMRF(Q)(sp);
