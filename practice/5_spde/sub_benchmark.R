fdf72cd3adc4838412d0639abb67bd48fa62a5f8
Roy Burstein <royburst@ihme.washington.edu>
2017-03-21 11:55:46 -0700

    s

--- practice/5_spde/sub_benchmark.R
+++ practice/5_spde/sub_benchmark.R
@@ -14,7 +14,7 @@ for(nc in c(10,50,100,500,1000,5000,10000,50000,100000)){
 
     message(nc); message(i)
 
-    qsub <- sprintf("qsub -e /share/temp/sgeoutput/royburst/errors -o /share/temp/sgeoutput/royburst/output -cwd -l mem_free=1G -P proj_geospatial -N job_%nc_%i r_shell.sh inla_tmb_benchmark.R %i %i",nc,i)
+    qsub <- sprintf("qsub -e /share/temp/sgeoutput/royburst/errors -o /share/temp/sgeoutput/royburst/output -cwd -l mem_free=1G -P proj_geospatial -N job_%i_%i r_shell.sh inla_tmb_benchmark.R %i %i",nc,i,nc,i)
 
     system(qsub)
 
